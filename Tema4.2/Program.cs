85d90b8139ace9de482f146d3c32f3208b5698d7
Mihai <Mihai@DESKTOP-MIJHQPJ>
2019-05-14 16:22:57 +0300

    commit

---
+++ Tema4.2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Queue Rand = new Queue();
+            //Rand.Enqueue("Mihai");
+            //Rand.Enqueue("Badarau");
+            //Rand.Enqueue("Andrei");
+            //Rand.Enqueue("Florin");
+            //Rand.Enqueue("Catalin");
+
+            //Console.WriteLine("Numarul total de elemenete din rand este: ", Rand);
+            //Console.WriteLine(Rand.Count);
+
+            //var arr = Rand.ToArray();
+
+            //foreach (object obj in arr)
+            //{
+            //    Console.WriteLine(obj);
+            //}
+
+            //Console.WriteLine("Elementul de varf din rand este: ", Rand);
+            //Console.WriteLine(Rand.Dequeue());
+
+            //Console.WriteLine("Numarul de elemente din rand este: {0} ", Rand.Count);
+
+            //////////////////////////////
+
+            Queue Q = new Queue(5);
+
+            Q.Enqueue(1);
+            Q.Enqueue(2);
+            Q.Enqueue(3);
+            Q.Enqueue(4);
+            Q.printQueue();
+            Q.Dequeue();
+
+            Stack myStack = new Stack();
+
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+            myStack.Push(4);
+            myStack.PrintStack();
+            Console.WriteLine("Item popped from Stack : {0}", myStack.Pop());
+            myStack.PrintStack();
+
+
+        }
+    }
+}
---
+++ Tema4.2/Queue.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema4._2
+{
+    class Queue
+    {
+        private int[] elemente;
+        private int front;
+        private int rear;
+        private int max;
+
+        public Queue(int size)
+        {
+            elemente = new int[size];
+            front = 0;
+            rear = -1;
+            max = size;
+        }
+
+        public void Enqueue(int item)
+        {
+            if (rear == max - 1)
+            {
+                Console.WriteLine("Queue Overflow");
+                return;
+            }
+            else
+            {
+                elemente[++rear] = item;
+            }
+
+        }
+
+        public int Dequeue()
+        {
+            if (front == rear + 1)
+            {
+                Console.WriteLine("Queue is Empty");
+                return -1;
+            }
+            else
+            {
+                Console.WriteLine(elemente[front] + " dequeued from queue");
+                int p = elemente[front++];
+                Console.WriteLine();
+                Console.WriteLine("Front item is {0}", elemente[front]);
+                Console.WriteLine("Rear item is {0} ", elemente[rear]);
+                return p;
+            }
+
+        }
+
+        public void printQueue()
+        {
+            if (front == rear + 1)
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+            else
+            {
+                for (int i = front; i <= rear; i++)
+                {
+                    Console.WriteLine(elemente[i] + " enqueued to queue");
+                }
+            }
+
+        }
+    }
+}
